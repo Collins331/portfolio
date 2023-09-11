7f650e79384b052e1a50fd6e2d112753ea0ad3a9
Collins Oduor Ochieng <125954874+Collins331@users.noreply.github.com>
2023-09-11 11:10:10 +0000

    updated my portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -24,14 +24,14 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Collins Ochieng",
+  title: "Junior Fullstack developer",
+  email: "ochiengcollinsoduor2@gmail.com",
+  gitHub: "https://github.com/Collins331",
+  instagram: "https://www.instagram.com/collins.linc/",
+  linkedIn: "https://www.linkedin.com/in/collins-ochieng-9b2303251/",
   medium: "",
-  twitter: "microsoft",
+  twitter: "https://twitter.com/Lincoln_331",
   youTube: "Code",
 };
 
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,7 +24,7 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a Software engineering student studying at ALX Africa. I enjoy creating unique and simplistic user interfaces in creative ways.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my Software engineering experience I continually look for new and better ways to make tech accessible by all.";
 
 const About = () => {
   return (

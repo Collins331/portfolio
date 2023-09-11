d76360f1932333e602b219e8b049576fcf905ac1
Collins Oduor Ochieng <125954874+Collins331@users.noreply.github.com>
2023-09-11 11:44:00 +0000

    Updates

--- index.975ef6c8.js
+++ index.975ef6c8.js
@@ -27090,11 +27090,11 @@ var _stylesCss = require("./styles.css");
     name: "Collins Ochieng",
     title: "Junior Fullstack developer",
     email: "ochiengcollinsoduor2@gmail.com",
-    gitHub: "https://github.com/Collins331",
-    instagram: "https://www.instagram.com/collins.linc/",
-    linkedIn: "https://www.linkedin.com/in/collins-ochieng-9b2303251/",
+    gitHub: "Collins331",
+    instagram: "collins.linc",
+    linkedIn: "collins-ochieng-9b2303251/",
     medium: "",
-    twitter: "https://twitter.com/Lincoln_331",
+    twitter: "Lincoln_331",
     youTube: "Code"
 };
 const primaryColor = "#4E567E";
